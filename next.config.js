0324134fa6fc00d3dae589d3d906fff814792f80
Jonathan Conway <jonathan.conway@gmail.com>
2024-02-26 19:22:17 +1100

    feat(setup): mdx (markdown + react pages)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
